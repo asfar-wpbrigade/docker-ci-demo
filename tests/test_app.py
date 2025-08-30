b7fe5f1980f9d77f1c5c0fdb06a9a7b773b71847
Ali Asfar <asfar.wpbrigade@gmail.com>
2025-08-30 12:27:05 +0500

    Initial commit

--- tests/test_app.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-def test_hello():
-    client = app.test_client()
-    resp = client.get("/")
-    assert resp.status_code == 200
-    assert resp.get_json() == {"message": "Hello, World!"}
